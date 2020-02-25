def26c2f913dc8c5b39cce99e68a01cb71c4ef29
Awosika Oladimeji <oladimejiawosika@gmail.com>
2020-02-25 11:24:49 -0800

    second commit

--- dictionary.py
+++ dictionary.py
@@ -1,10 +1,13 @@
 import json
-#help(json.load)
 
 data = json.load(open("data.json", 'r'))
 
 def meaning(word):
-    return data[word]
+    word=word.lower()
+    if word in data:
+        return data[word]
+    else:
+        print("This word doesn't exist, please check to confirm")
 
 word = input('Enter the word you will like to search: ')
 
