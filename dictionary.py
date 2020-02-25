5cb65fdf7ecbeb4558704bff0922bb52d7108b82
Awosika Oladimeji <oladimejiawosika@gmail.com>
2020-02-25 12:44:47 -0800

    third commit

--- dictionary.py
+++ dictionary.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json", 'r'))
 
@@ -6,6 +7,12 @@ def meaning(word):
     word=word.lower()
     if word in data:
         return data[word]
+    elif len(get_close_matches(word, data.keys())) > 0:
+        yn = input ("Did you mean %s instead? If you mean Yes enter Y enter N for No: " % get_close_matches(word, data.keys())[0])
+        if yn == "Y":
+            return data[get_close_matches(word, data.keys())[0]]
+        elif yn == "N":
+            return "Sorry the word doesn't exist"
     else:
         print("This word doesn't exist, please check to confirm")
 
